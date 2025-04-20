bacda32bcc883bae6278d25d13904d0e87693386
rosyimuth <rosyimuth@gmail.com>
2025-04-20 20:53:48 +0700

    Coba vercel

--- index.py
+++ index.py
@@ -6,10 +6,7 @@ app = Flask(__name__)
 def todoit():
     if request.method == 'POST':
         # Jika ada data yang dikirim melalui metode POST
-        data = request.form.get('some_data')
+        data = request.form['data']
         # Lakukan sesuatu dengan data yang diterima
         return f"Data yang dikirim: {data}"
     return render_template('index.html')
-
-if __name__ == '__main__':
-    app.run(debug=True)
