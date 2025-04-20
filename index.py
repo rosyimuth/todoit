affbae277842a218fe61f06af56e02b77ea0b02b
rosyimuth <rosyimuth@gmail.com>
2025-04-20 20:57:00 +0700

    Coba vercel

--- index.py
+++ index.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template, request
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="../templates", static_folder="../static")
 
 @app.route('/', methods=['GET', 'POST'])
 def todoit():
     if request.method == 'POST':
-        # Jika ada data yang dikirim melalui metode POST
-        data = request.form['data']
-        # Lakukan sesuatu dengan data yang diterima
-        return f"Data yang dikirim: {data}"
-    return render_template('index.html')
+        data = request.form['data']  # Menangkap data yang dikirim
+        # Mengirim data ke template untuk ditampilkan
+        return render_template('index.html', data=data)
+    return render_template('index.html', data=None)  # Jika GET, kirim None untuk data
+
+if __name__ == '__main__':
+    app.run(debug=True)
