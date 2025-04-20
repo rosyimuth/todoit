92dd881fadc3a2964e897dbb3a5194057854c671
rosyimuth <rosyimuth@gmail.com>
2025-04-20 20:31:36 +0700

    Coba vercel

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    # Mengarahkan ke file templates/index.html
+    # Menggunakan render_template untuk merender file templates/index.html
     return render_template('index.html')
 
 if __name__ == '__main__':
