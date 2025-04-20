89e22d00788eeda6bc23d6d83ff6bbcb6166ddda
rosyimuth <rosyimuth@gmail.com>
2025-04-20 20:47:18 +0700

    Coba vercel

--- index.py
+++ index.py
@@ -1,11 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    # Menggunakan render_template untuk merender file templates/index.html
+@app.route('/', methods=['GET', 'POST'])
+def todoit():
+    if request.method == 'POST':
+        # Jika ada data yang dikirim melalui metode POST
+        data = request.form.get('some_data')
+        # Lakukan sesuatu dengan data yang diterima
+        return f"Data yang dikirim: {data}"
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(True)
+    app.run(debug=True)
